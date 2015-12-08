0df95cc880d3ea8503a9ce61df65b453d17ce0b0
suprakash.bh@gmail.com <suprakash.bh@gmail.com>
2015-12-08 10:33:58 +0530

    test

--- src/com/infy/ms/rest/fc/ForecastMsApplication.java
+++ src/com/infy/ms/rest/fc/ForecastMsApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ForecastMsApplication {
 	
 	public static void main(String[] args) {
+		System.out.println("test forecast microservice-------");
 		SpringApplication.run(ForecastMsApplication.class, args);
 	}
 
